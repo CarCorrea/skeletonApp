f1f1924d31b08dec81b66793033d3b28261c102b
CarCorrea <carlos.correa.zapata@gmsil.com>
2021-09-05 22:16:45 -0300

    Added login and home pages and functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  userName: any;
+  user={
+    name: "",
+    lastName: "",
+    edLevel: "",
+    birthDate: ""
+  };
 
+  constructor(public toastController: ToastController, private activatedRoute: ActivatedRoute, private router: Router) {
+
+  this.activatedRoute.queryParams.subscribe(params => {
+    if(this.router.getCurrentNavigation().extras.state){
+      this.userName = this.router.getCurrentNavigation().extras.state.user;
+      console.log(this.userName);
+    }
+  });
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  show(){
+    this.showData("Su nombre es: " + this.user.name + " " + this.user.lastName);
+  }
+
+  async showData(msg: string){
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  clean(){
+    this.user.name = "";
+    this.user.lastName = "";
+    this.user.edLevel = "";
+    this.user.birthDate = "";
+  }
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  user={
+    userName:"",
+    password:""
+  }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  ingresar() {
+    let navigationExtras: NavigationExtras ={
+      state:{user: this.user.userName}
+    };
+    this.router.navigate(['/home'], navigationExtras);
+  };
 }
